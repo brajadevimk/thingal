db0af0111265c946f1ee74b13cf0f058fae39274
brajadevimk <brajadevi1976@gmail.com>
2025-06-17 11:15:47 +0530

    changed it back

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
-                  'Break Fast fast :',
+                  'Break Fast :',
                   style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
                 Container(
